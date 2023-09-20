f50e8b56ed7e5d5cc3463f980a4d624f05704056
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-09-20 07:09:39 -0300

    Create AppConfigHelperClass.cs
    - Classe que auxilia a acessar as configurações armazenadas do aplicativo

---
+++ NutriLensSolution/NutriLens/Entities/AppConfigHelperClass.cs
@@ -0,0 +1,36 @@
+﻿using NutriLens.Models;
+using NutriLens.Services;
+using ExceptionLibrary;
+
+namespace NutriLens.Entities
+{
+    public static class AppConfigHelperClass
+    {
+        public enum ConfigItems
+        {
+            EnergeticUnit
+        }
+
+        private static EnergeticUnit? _energeticUnit;
+
+        public static EnergeticUnit EnergeticUnit
+        {
+            get
+            {
+                if (_energeticUnit == null)
+                {
+                    try
+                    {
+                        _energeticUnit = (EnergeticUnit?)(ConfigItems)ViewServices.AppDataManager.GetItem<int>(ConfigItems.EnergeticUnit);
+                    }
+                    catch (NotFoundException)
+                    {
+                        _energeticUnit = EnergeticUnit.kcal;
+                    }
+                }
+
+                return (EnergeticUnit)_energeticUnit;
+            }
+        }
+    }
+}
