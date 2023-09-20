80d498b5c6906941f7a74cceedcaeb5e5de5444f
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-09-20 07:09:03 -0300

    Create AppDataHelperClass.cs
    - Classe que auxilia a acessar os dados armazenados do aplicativo

---
+++ NutriLensSolution/NutriLens/Entities/AppDataHelperClass.cs
@@ -0,0 +1,68 @@
+﻿using ExceptionLibrary;
+using NutriLens.Models;
+using NutriLens.Services;
+
+namespace NutriLens.Entities
+{
+    public static class AppDataHelperClass
+    {
+        public enum DataItems
+        {
+            MealList
+        }
+
+        private static List<Meal> _mealList;
+
+        private static List<Meal> MealList
+        {
+            get
+            {
+                if (_mealList == null)
+                {
+                    try
+                    {
+                        _mealList = ViewServices.AppDataManager.GetItem<List<Meal>>(DataItems.MealList);
+                    }
+                    catch (NotFoundException)
+                    {
+                        _mealList = new List<Meal>();
+                    }
+                }
+
+                return _mealList;
+            }
+        }
+
+        public static void AddMeal(Meal meal)
+        {
+            MealList.Add(meal);
+            ViewServices.AppDataManager.SetItem(DataItems.MealList, MealList);
+        }
+
+        public static List<Meal> GetAllMeals()
+        {
+            return MealList;
+        }
+
+        public static List<Meal> GetTodayMeals()
+        {
+            return MealList
+                .Where(x => x.DateTime.Date == DateTime.Today)
+                .ToList();
+        }
+
+        public static List<Meal> GetMealsByDate(DateTime date)
+        {
+            return MealList
+                .Where(x => x.DateTime.Date == date.Date)
+                .ToList();
+        }
+
+        public static List<Meal> GetMealsByDateRange(DateTime startDate, DateTime endDate)
+        {
+            return MealList
+                .Where(x => x.DateTime >= startDate && x.DateTime <= endDate)
+                .ToList();
+        }
+    }
+}
