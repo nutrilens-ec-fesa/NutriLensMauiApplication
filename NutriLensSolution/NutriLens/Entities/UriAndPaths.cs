32ca96dc7f6cb1318cce01574b5e39c70d5f6f21
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-09-20 07:10:41 -0300

    Update UriAndPaths.cs
    - Criação do caminho de arquivo para os dados do aplicativo

--- NutriLensSolution/NutriLens/Entities/UriAndPaths.cs
+++ NutriLensSolution/NutriLens/Entities/UriAndPaths.cs
@@ -3,6 +3,9 @@
     public static class UriAndPaths
     {
         private static readonly string _appConfigFile = "appconfig.json";
+        private static readonly string _appDataFile = "appdata.json";
+
         public static readonly string appConfigurationPath = Path.Combine(FileSystem.AppDataDirectory, _appConfigFile);
+        public static readonly string appDataPath = Path.Combine(FileSystem.AppDataDirectory, _appDataFile);
     }
 }
