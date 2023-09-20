ca5a3de433fc1c0c22ad169ab54d0e641c97f64f
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-09-20 07:09:58 -0300

    Create Constants.cs
    - Classe que constantes que vão ser utilizadas na aplicação

---
+++ NutriLensSolution/NutriLens/Entities/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace NutriLens.Entities
+{
+    public static class Constants
+    {
+        public static readonly string kcalUnit = "kcal";
+        public static readonly string kJUnit = "kJ";
+        public static readonly double kcalToKJFactor = 4.184;
+    }
+}
