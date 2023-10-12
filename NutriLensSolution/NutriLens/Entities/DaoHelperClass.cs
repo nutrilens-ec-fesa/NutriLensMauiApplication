be1f031afae9f6ffe1c101e11f38693f0b79cfce
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-10-12 09:49:17 -0300

    Criação DaoHelperClass + recursos OpenAi (GPT)

---
+++ NutriLensSolution/NutriLens/Entities/DaoHelperClass.cs
@@ -0,0 +1,24 @@
+﻿using NutriLens.Models;
+using static NutriLens.Entities.OpenAiEntity;
+
+namespace NutriLens.Entities
+{
+    public static class DaoHelperClass
+    {
+        public static string GetNutritionalInfo(FoodItem foodItem)
+        {
+            OpenAiInputModel openAiInputModel = new()
+            {
+                SystemPrompt = "Be direct. Answer only the nutritional info in json format including units. Consider the average size/type.",
+                UserPrompt = foodItem.ToString(),
+                Temperature = 1,
+                TopP = 1,
+                FrequencyPenalty = 0,
+                PresencePenalty = 0
+            };
+
+            OpenAiResponse openAiResponse = OpenAiQuery(OpenAiModel.Gpt3dot5Turbo, openAiInputModel);
+            return openAiResponse.GetResponseMessage();
+        }
+    }
+}
