bcb8e05481ee5ece66c90cbadcfb501cbdad00f8
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-10-14 14:31:04 -0300

    Create GptNutritionalInfo.cs
    - Classe que recebe o json gerado pelo ChatGpt, pode ser que ainda precisem ser adicionados novos campos.

---
+++ NutriLensSolution/NutriLens/Models/GptNutritionalInfo.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NutriLens.Models
+{
+    public partial class GptNutritionalInfo
+    {
+        [JsonProperty("calories")]
+        public string CaloriesRaw { get; set; }
+
+        [JsonProperty("carbohydrates")]
+        public string CarbohydratesRaw { get; set; }
+
+        [JsonProperty("cholesterol")]
+        public string CholesterolRaw { get; set; }
+
+        [JsonProperty("protein")]
+        public string ProteinRaw { get; set; }
+
+        [JsonProperty("fat")]
+        public string FatRaw { get; set; }
+
+        [JsonProperty("saturated_fat")]
+        public string SaturatedFatRaw { get; set; }
+
+        [JsonProperty("sodium")]
+        public string SodiumRaw { get; set; }
+
+        [JsonProperty("sugar")]
+        public string SugarRaw { get; set; }
+
+        [JsonProperty("total_fat")]
+        public string TotalFatRaw { get; set; }
+
+        [JsonProperty("fiber")]
+        public string FiberRaw { get; set; }
+
+        public double CaloriesValue { get => double.Parse(ValueRegex().Match(CaloriesRaw).Value); }
+
+        [GeneratedRegex("[\\d]*([\\.,][\\d]+)?")]
+        private static partial Regex ValueRegex();
+    }
+}
