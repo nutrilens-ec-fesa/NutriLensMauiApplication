cc0682654a8ab2172a0ab5b02e7b1092ab969ecd
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-10-14 14:29:54 -0300

    UriAndPaths - Nome do diretório das imagens do Mongo

--- NutriLensSolution/NutriLens/Entities/UriAndPaths.cs
+++ NutriLensSolution/NutriLens/Entities/UriAndPaths.cs
@@ -4,8 +4,10 @@
     {
         private static readonly string _appConfigFile = "appconfig.json";
         private static readonly string _appDataFile = "appdata.json";
+        private static readonly string _databasePicturesDirectory = "database";
 
         public static readonly string appConfigurationPath = Path.Combine(FileSystem.AppDataDirectory, _appConfigFile);
         public static readonly string appDataPath = Path.Combine(FileSystem.AppDataDirectory, _appDataFile);
+        public static readonly string databasePicturesPath = Path.Combine(FileSystem.AppDataDirectory, _databasePicturesDirectory);
     }
 }
