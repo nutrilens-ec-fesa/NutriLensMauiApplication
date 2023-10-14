695315be902309845aeea6630d3cb14daf522f8e
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-10-14 14:32:29 -0300

    Update MongoImage.cs
    - Adicionado campo de nome da imagem.

--- NutriLensSolution/NutriLens/Models/MongoImage.cs
+++ NutriLensSolution/NutriLens/Models/MongoImage.cs
@@ -5,6 +5,7 @@ namespace NutriLens.Models
     public class MongoImage
     {
         public BsonObjectId Id { get; set; }
+        public string FileName { get; set; }
         public byte[] ImageBytes { get; set; }
     }
 }
