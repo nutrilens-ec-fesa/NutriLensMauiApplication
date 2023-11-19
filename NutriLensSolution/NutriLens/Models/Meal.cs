50a86a5c992a0267e1e2d7e855b37f122cf87743
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-11-19 15:15:50 -0300

    Documentação da classe Meal

--- NutriLensSolution/NutriLens/Models/Meal.cs
+++ NutriLensSolution/NutriLens/Models/Meal.cs
@@ -1,9 +1,23 @@
 ﻿namespace NutriLens.Models
 {
+    /// <summary>
+    /// Representa um modelo de refeição
+    /// </summary>
     public class Meal
     {
+        /// <summary>
+        /// Data e hora da refeição
+        /// </summary>
         public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Nome da refeição
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Itens alimentícios presentes na refeição
+        /// </summary>
         public List<FoodItem> FoodItems { get; set; }
 
         public override string ToString()
