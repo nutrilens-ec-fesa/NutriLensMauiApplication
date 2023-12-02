78d81129a9e32882ac99b5390c3285dfd897071e
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-12-02 14:34:59 -0300

    Update AppDataHelperClass
    - Recursos para dados de usuário
    - Documentação da classe

--- NutriLensSolution/NutriLens/Entities/AppDataHelperClass.cs
+++ NutriLensSolution/NutriLens/Entities/AppDataHelperClass.cs
@@ -4,15 +4,33 @@ using NutriLens.Services;
 
 namespace NutriLens.Entities
 {
+    /// <summary>
+    /// Classe que gerencia o acesso aos dados salvos no aplicativo
+    /// </summary>
     public static class AppDataHelperClass
     {
+        /// <summary>
+        /// Enumerador de tipos de dados
+        /// </summary>
         public enum DataItems
         {
-            MealList
+            /// <summary>
+            /// Lista de refeições
+            /// </summary>
+            MealList,
+
+            /// <summary>
+            /// Informações do usuário
+            /// </summary>
+            UserInfo
         }
 
         private static List<Meal> _mealList;
+        private static UserInfo _userInfo;
 
+        /// <summary>
+        /// Lista de refeições
+        /// </summary>
         private static List<Meal> MealList
         {
             get
@@ -33,17 +51,60 @@ namespace NutriLens.Entities
             }
         }
 
+        /// <summary>
+        /// Informações do usuário
+        /// </summary>
+        public static UserInfo UserInfo
+        {
+            get
+            {
+                if (_userInfo == null)
+                {
+                    try
+                    {
+                        _userInfo = ViewServices.AppDataManager.GetItem<UserInfo>(DataItems.UserInfo);
+                    }
+                    catch (NotFoundException)
+                    {
+
+                    }
+                }
+
+                if (_userInfo == null)
+                    _userInfo = new UserInfo();
+
+                return _userInfo;
+            }
+        }
+
+        /// <summary>
+        /// Verifica se existe informações de usuário salvas no aplicativo
+        /// </summary>
+        public static bool HasUserInfo { get => UserInfo != null; }
+
+        /// <summary>
+        /// Adiciona uma nova refeição
+        /// </summary>
+        /// <param name="meal"></param>
         public static void AddMeal(Meal meal)
         {
             MealList.Add(meal);
             ViewServices.AppDataManager.SetItem(DataItems.MealList, MealList);
         }
 
+        /// <summary>
+        /// Retorna todas as refeições salvas
+        /// </summary>
+        /// <returns></returns>
         public static List<Meal> GetAllMeals()
         {
             return MealList;
         }
 
+        /// <summary>
+        /// Retorna todas as refeições da data atual
+        /// </summary>
+        /// <returns></returns>
         public static List<Meal> GetTodayMeals()
         {
             return MealList
@@ -51,6 +112,11 @@ namespace NutriLens.Entities
                 .ToList();
         }
 
+        /// <summary>
+        /// Retorna todas as refeições em um data específica
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public static List<Meal> GetMealsByDate(DateTime date)
         {
             return MealList
@@ -58,11 +124,36 @@ namespace NutriLens.Entities
                 .ToList();
         }
 
+        /// <summary>
+        /// Retorna as refeições que aconteceram entre um intervalo específico
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
         public static List<Meal> GetMealsByDateRange(DateTime startDate, DateTime endDate)
         {
             return MealList
                 .Where(x => x.DateTime >= startDate && x.DateTime <= endDate)
                 .ToList();
         }
+
+        /// <summary>
+        /// Seta as informações de usuário
+        /// </summary>
+        /// <param name="userInfo"></param>
+        public static void SetUserInfo(UserInfo userInfo)
+        {
+            ViewServices.AppDataManager.SetItem(DataItems.UserInfo, userInfo);
+            _userInfo = userInfo;
+        }
+
+        public static double GetEnergeticDiaryObjective()
+        {
+            if (AppConfigHelperClass.EnergeticUnit == EnergeticUnit.kJ)
+                return UserInfo.KiloCaloriesDiaryObjective * Constants.kcalToKJFactor;
+            else
+                return UserInfo.KiloCaloriesDiaryObjective;
+        }
+
     }
 }
