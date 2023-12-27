ec0fc4790c0db0261d4375e62f4ba440c371ad63
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-12-27 17:28:49 -0300

    Update Constants.cs
    - Recursos para separador de decimal

--- NutriLensSolution/NutriLens/Entities/Constants.cs
+++ NutriLensSolution/NutriLens/Entities/Constants.cs
@@ -5,5 +5,8 @@
         public static readonly string kcalUnit = "kcal";
         public static readonly string kJUnit = "kJ";
         public static readonly double kcalToKJFactor = 4.184;
+
+        public static readonly char cultureDecimalSeparator = Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+        public static readonly char decimalToBeReplaced = cultureDecimalSeparator == ',' ? '.' : ',';
     }
 }
