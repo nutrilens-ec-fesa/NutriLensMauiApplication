3eaceb4997a50aac4a9ef3d2e1e6424aca9ab2f2
Carlos Henrique <carlos_henrique97@outlook.com.br>
2023-12-27 17:26:05 -0300

    Create Login.cs
    - Modelo para realizar login na aplicação

---
+++ NutriLensSolution/NutriLens/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace NutriLens.Models
+{
+    public class Login
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
