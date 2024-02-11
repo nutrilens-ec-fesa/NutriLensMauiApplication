fa90930f24d3777c3d2b8bcd3bb7b748a48f9789
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-02-11 11:58:33 -0300

    Create ErrorViewModel.cs
    - ErrorViewModel do template

---
+++ NutriLensSolution/NutriLensWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NutriLensWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
