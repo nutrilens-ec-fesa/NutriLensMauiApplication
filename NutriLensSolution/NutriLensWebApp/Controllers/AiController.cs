baf265a1a7dfc6089dd020b5cd581de60ed80796
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-02-24 15:40:43 -0300

    Update AiController.cs
    - Update rotas de utilização do ChatGPT
    - Criação de rotas para coleta/inserção de prompts do ChatGPT

--- NutriLensSolution/NutriLensWebApp/Controllers/AiController.cs
+++ NutriLensSolution/NutriLensWebApp/Controllers/AiController.cs
@@ -1,6 +1,8 @@
 ﻿using ExceptionLibrary;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NutriLensClassLibrary.Models;
+using NutriLensWebApp.Interfaces;
 using NutriLensWebApp.Models;
 using static NutriLensWebApp.Entities.OpenAiEntity;
 
@@ -16,14 +18,19 @@ namespace NutriLensWebApp.Controllers
     public class AiController : ControllerBase
     {
         [HttpPost, Route("v1/DetectFoodByUrl")]
-        public IActionResult DetectFoodByUrl([FromBody] string url)
+        public IActionResult DetectFoodByUrl([FromBody] string url, [FromServices] IOpenAiPrompt openAiPromptRepo)
         {
+            // UserPrompt = "Analise essa foto. Retorne em json no formato: [{Item:item,Quantidade:g},{Item:item,Quantidade:g}]",
+            // SystemPrompt = "Você identifica itens alimentícios de uma refeição, trazendo em json uma lista de item e quantidade em gramas ou ml, de cada item. Eu sei que as quantidades são aproximadas, portanto, não é necessário escrever aproximadamente ou coisa do tipo. Você é objetivo e não retorna mais nada além do solicitado.",
+
             try
             {
-                OpenAiVisionInputModel inputModel = new OpenAiVisionInputModel
+                OpenAiPrompt openAiPrompt = openAiPromptRepo.GetLast();
+
+                OpenAiVisionInputModel inputModel = new()
                 {
-                    UserPrompt = "Analise essa foto.",
-                    SystemPrompt = "Você identifica itens alimentícios de uma refeição, trazendo seu nome e a quantidade em gramas ou ml, em itens. Eu sei que as quantidades são aproximadas, portanto, não é necessário escrever aproximadamente ou coisa do tipo. Você é objetivo e não retorna mais nada além do solicitado.",
+                    UserPrompt = openAiPrompt.UserPrompt,
+                    SystemPrompt = openAiPrompt.SystemPrompt,
                     MaxTokens = 300,
                     Base64 = false,
                     Url = url
@@ -33,21 +40,23 @@ namespace NutriLensWebApp.Controllers
 
                 return Ok(response.GetResponseMessage());
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(ExceptionManager.ExceptionMessage(ex));
             }
         }
 
         [HttpPost, Route("v1/DetectFoodByBase64Image")]
-        public IActionResult DetectFoodByBase64Image([FromBody] string base64Image)
+        public IActionResult DetectFoodByBase64Image([FromBody] string base64Image, [FromServices] IOpenAiPrompt openAiPromptRepo)
         {
             try
             {
-                OpenAiVisionInputModel inputModel = new OpenAiVisionInputModel
+                OpenAiPrompt openAiPrompt = openAiPromptRepo.GetLast();
+
+                OpenAiVisionInputModel inputModel = new()
                 {
-                    UserPrompt = "Analise essa foto.",
-                    SystemPrompt = "Você identifica itens alimentícios de uma refeição, trazendo seu nome e a quantidade em gramas ou ml, em itens. Eu sei que as quantidades são aproximadas, portanto, não é necessário escrever aproximadamente ou coisa do tipo. Você é objetivo e não retorna mais nada além do solicitado.",
+                    UserPrompt = openAiPrompt.UserPrompt,
+                    SystemPrompt = openAiPrompt.SystemPrompt,
                     MaxTokens = 300,
                     Base64 = false,
                     Url = $"data:image/jpeg;base64,{base64Image}"
@@ -56,6 +65,34 @@ namespace NutriLensWebApp.Controllers
                 OpenAiResponse response = OpenAiQuery(OpenAiModel.Gpt4VisionPreview, inputModel);
 
                 return Ok(response.GetResponseMessage());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ExceptionManager.ExceptionMessage(ex));
+            }
+        }
+
+        [HttpGet, Route("v1/GetActualGpt4VisionPrompt")]
+        public IActionResult GetActualGpt4VisionPrompt([FromServices] IOpenAiPrompt openAiPromptRepo)
+        {
+            try
+            {
+                return Ok(openAiPromptRepo.GetLast());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ExceptionManager.ExceptionMessage(ex));
+            }
+        }
+
+        [HttpPost, Route("v1/InsertNewGpt4VisionPrompt")]
+        public IActionResult InsertNewPrompt([FromBody] OpenAiPrompt openAiPrompt,
+            [FromServices] IOpenAiPrompt openAiPromptRepo)
+        {
+            try
+            {
+                openAiPromptRepo.InsertNew(openAiPrompt);
+                return Created(string.Empty, string.Empty);
             }
             catch (Exception ex)
             {
