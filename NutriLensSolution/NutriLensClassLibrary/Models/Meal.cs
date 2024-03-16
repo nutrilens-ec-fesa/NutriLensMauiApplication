90728bf53c194786508a4618b6a98389fa5c5caa
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-03-16 13:31:32 -0300

    Meal em NutriLensClassLibrary

---
+++ NutriLensSolution/NutriLensClassLibrary/Models/Meal.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using NutriLensClassLibrary.Models;
+
+namespace NutriLens.Models
+{
+    /// <summary>
+    /// Representa um modelo de refeição
+    /// </summary>
+    public class Meal
+    {
+        /// <summary>
+        /// Id da refeição no Mongo
+        /// </summary>
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Propriedade que define o usuário que criou essa refeição
+        /// </summary>
+        public string UserIdentifier { get; set; }
+
+        /// <summary>
+        /// Data e hora da refeição
+        /// </summary>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Nome da refeição
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Itens alimentícios presentes na refeição
+        /// </summary>
+        public List<FoodItem> FoodItems { get; set; }
+
+        public override string ToString()
+        {
+            if(FoodItems.Count > 1)
+                return $"{DateTime} - {Name} - {FoodItems.Count} itens";
+            else
+                return $"{DateTime} - {Name} - {FoodItems.Count} item";
+        }
+    }
+}
