9960a93693635ea458b98a05e57d9103e600e3e5
Johnny Messias Paulino <90730926+JohnnyMessiasPaulino@users.noreply.github.com>
2024-03-17 17:05:49 -0300

    Create TacoItem.cs

---
+++ NutriLensSolution/NutriLensClassLibrary/Models/TacoItem.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace NutriLensClassLibrary.Models
+{
+    public class TacoItem
+    {
+        [BsonId, JsonIgnore]
+        public ObjectId _id { get; set; }
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public int? EnergiaKj { get; set; }
+        public int? EnergiaKcal { get; set; }
+        public double? Proteina { get; set; }
+        public double? Lipideos { get; set; }
+        public double? Colesterol { get; set; }
+        public double? Carboidrato { get; set; }
+        public double? FibraAlimentar { get; set; }
+        public double? Cinzas { get; set; }
+        public double? Calcio { get; set; }
+        public double? Magnesio { get; set; }
+        public double? Manganes { get; set; }
+        public double? Fosforo { get; set; }
+        public double? Ferro { get; set; }
+        public double? Sodio { get; set; }
+        public double? Potassio { get; set; }
+        public double? Cobre { get; set; }
+        public double? Zinco { get; set; }
+        public double? Retinol { get; set; }
+        public double? RE { get; set; }
+        public double? RAE { get; set; }
+        public double? Tiamina { get; set; }
+        public double? Riboflavina { get; set; }
+        public double? Niacina { get; set; }
+        public double? VitaminaC { get; set; }
+
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+    }
+}
