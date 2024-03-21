d0b3932c7cb42fbe651d0ff462de4ad683526843
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-03-21 10:37:06 -0300

    Update OpenAiVisionInputModel.cs
    - Adição de JsonProperties

--- NutriLensSolution/NutriLensWebApp/Models/OpenAiVisionInputModel.cs
+++ NutriLensSolution/NutriLensWebApp/Models/OpenAiVisionInputModel.cs
@@ -1,11 +1,18 @@
-﻿namespace NutriLensWebApp.Models
+﻿using Newtonsoft.Json;
+
+namespace NutriLensWebApp.Models
 {
     public class OpenAiVisionInputModel
     {
+        [JsonProperty("systemPrompt")]
         public string SystemPrompt { get; set; }
+        [JsonProperty("userPrompt")]
         public string UserPrompt { get; set; }
+        [JsonProperty("maxTokens")]
         public int? MaxTokens { get; set; }
+        [JsonProperty("url")]
         public string Url { get; set; }
+        [JsonProperty("base64")]
         public bool Base64 { get; set; }
     }
 }
