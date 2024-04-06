5084502475aa52f9c1c8dbff5ac5d691b1a60d4a
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-04-06 15:50:20 -0300

    Update MongoImage.cs
    - Novas propriedades

--- NutriLensSolution/NutriLensClassLibrary/Models/MongoImage.cs
+++ NutriLensSolution/NutriLensClassLibrary/Models/MongoImage.cs
@@ -10,7 +10,10 @@ namespace NutriLensClassLibrary.Models
         public string UserIdentifier { get; set; }
         public byte[] ImageBytes { get; set; }
         public string VisionRawResult { get; set; }
+        public string HumanResult { get; set; }
         public string FileName { get; set; }
+        public int? TotalItems { get; set; }
+        public int? TotalOkItems { get; set; }
         public DateTime DateTime { get; set; }
     }
 }
