65d25a605c4772f0c1f3471bd220c55e0edfb65b
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-04-09 22:44:59 -0300

    Criação StringLibrary
    - Biblioteca para recursos de string

---
+++ NutriLensSolution/StringLibrary/StringFunctions.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace StringLibrary
+{
+    public static class StringFunctions
+    {
+        /// <summary>
+        /// Remove acentos e caracteres especiais
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var character in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(character);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(character);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+            //if (string.IsNullOrEmpty(text))
+            //    return text;
+
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if (text[i] > 255)
+            //        sb.Append(text[i]);
+            //    else
+            //        sb.Append(s_Diacritics[text[i]]);
+            //}
+
+            //return sb.ToString();
+        }
+
+        /// <summary>
+        /// Verifica se duas strings são iguais, desconsiderando acentos
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool CompareString(string a, string b)
+        {
+            return RemoveDiacritics(a) == RemoveDiacritics(b);
+        }
+    }
+}
