977872323f508ea54ce2779050cace441636f6b7
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-04-09 22:46:43 -0300

    Create SimpleFoodItem.cs
    - Classe para item/quantidade de alimentos

---
+++ NutriLensSolution/NutriLensClassLibrary/Models/SimpleFoodItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriLensClassLibrary.Models
+{
+    public class SimpleFoodItem
+    {
+        public string Item { get; set; }
+        public int Quantity { get; set; }
+
+        public SimpleFoodItem(string item, int quantity)
+        {
+            Item = item;
+            Quantity = quantity;
+        }
+    }
+}
