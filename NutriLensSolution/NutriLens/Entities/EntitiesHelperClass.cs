8d8ca6c57a5d6a0e749b672766f1ecdc510efdf4
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-04-14 16:45:36 -0300

    Update EntitiesHelperClass.cs
    - Migração do método para execução de análise dos modelos de IA.
    - Criação de método para executar a obtenção dos itens alimentícios a partir da descrição obtida por comando de voz.

--- NutriLensSolution/NutriLens/Entities/EntitiesHelperClass.cs
+++ NutriLensSolution/NutriLens/Entities/EntitiesHelperClass.cs
@@ -64,5 +64,193 @@ namespace NutriLens.Entities
                 _ => default
             };
         }
+
+        public static async Task<List<FoodItem>> GetAiAnalysisByMongoImageId(string mongoImageId)
+        {
+            EntitiesHelperClass.ShowLoading("Realizando análise dos alimentos...");
+
+            string gptResult = string.Empty;
+            string geminiResult = string.Empty;
+
+            #region Analise usando modelos
+
+            TaskCompletionSource<bool> tcsGpt = new TaskCompletionSource<bool>();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    gptResult = DaoHelperClass.GetOpenAiFoodVisionAnalisysByImageId(mongoImageId);
+                    tcsGpt.SetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcsGpt.SetResult(false);
+                }
+
+            });
+
+            TaskCompletionSource<bool> tcsGemini = new TaskCompletionSource<bool>();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    geminiResult = DaoHelperClass.GetGeminiAiFoodVisionAnalisysByImageId(mongoImageId);
+                    tcsGemini.SetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcsGemini.SetResult(false);
+                }
+            });
+
+            // Aguarda as consultas terminarem
+            do
+            {
+                await Task.Delay(1000);
+
+                Console.WriteLine("GPT: " + tcsGpt.Task.IsCompleted);
+                Console.WriteLine("Gemini: " + tcsGemini.Task.IsCompleted);
+
+            } while (!tcsGpt.Task.IsCompleted || !tcsGemini.Task.IsCompleted);
+
+            #endregion
+
+            List<SimpleFoodItem> gptSimpleFoodItems = JsonToFoodItemsParser.Parse(gptResult);
+            List<SimpleFoodItem> geminiSimpleFoodItems = JsonToFoodItemsParser.Parse(geminiResult);
+
+            List<FoodItem> gptFoodItems = null;
+            List<FoodItem> geminiFoodItems = null;
+
+            try
+            {
+                AppDataHelperClass.GetStringTacoItemsBySimpleFoodItems(gptSimpleFoodItems, out gptFoodItems);
+            }
+            catch (Exception ex)
+            {
+                //await EntitiesHelperClass.CloseLoading();
+                //await ViewServices.PopUpManager.PopErrorAsync($"Houve algum problema com a análise da foto dos alimentos. {Environment.NewLine} Retorno da API: {gptResult}. {Environment.NewLine}" + ExceptionManager.ExceptionMessage(ex));
+                //return;
+            }
+
+            try
+            {
+                AppDataHelperClass.GetStringTacoItemsBySimpleFoodItems(geminiSimpleFoodItems, out geminiFoodItems);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            await EntitiesHelperClass.CloseLoading();
+
+            bool gptOk = gptFoodItems != null && gptFoodItems.Count > 0;
+            bool geminiOk = geminiFoodItems != null && geminiFoodItems.Count > 0;
+
+            Console.WriteLine("ChatGPT: " + (gptOk ? "OK" : "Falha"));
+            Console.WriteLine("Gemini: " + (geminiOk ? "OK" : "Falha"));
+
+            if (gptOk)
+                return gptFoodItems;
+            else if (geminiOk)
+                return geminiFoodItems;
+            else
+                return null;
+        }
+
+        public static async Task<List<FoodItem>> GetAiAnalysisByMealDescription(string mealDescription)
+        {
+            EntitiesHelperClass.ShowLoading("Identificando os alimentos...");
+
+            string gptResult = string.Empty;
+            string geminiResult = string.Empty;
+
+            #region Analise usando modelos
+
+            TaskCompletionSource<bool> tcsGpt = new TaskCompletionSource<bool>();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    gptResult = DaoHelperClass.GetOpenAiFoodItemsJsonByMealDescription(mealDescription);
+                    tcsGpt.SetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcsGpt.SetResult(false);
+                }
+
+            });
+
+            TaskCompletionSource<bool> tcsGemini = new TaskCompletionSource<bool>();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    geminiResult = DaoHelperClass.GetGeminiFoodItemsJsonByMealDescription(mealDescription);
+                    tcsGemini.SetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcsGemini.SetResult(false);
+                }
+            });
+
+            // Aguarda as consultas terminarem
+            do
+            {
+                await Task.Delay(1000);
+
+                Console.WriteLine("GPT: " + tcsGpt.Task.IsCompleted);
+                Console.WriteLine("Gemini: " + tcsGemini.Task.IsCompleted);
+
+            } while (!tcsGpt.Task.IsCompleted || !tcsGemini.Task.IsCompleted);
+
+            #endregion
+
+            List<SimpleFoodItem> gptSimpleFoodItems = JsonToFoodItemsParser.Parse(gptResult);
+            List<SimpleFoodItem> geminiSimpleFoodItems = JsonToFoodItemsParser.Parse(geminiResult);
+
+            List<FoodItem> gptFoodItems = null;
+            List<FoodItem> geminiFoodItems = null;
+
+            try
+            {
+                AppDataHelperClass.GetStringTacoItemsBySimpleFoodItems(gptSimpleFoodItems, out gptFoodItems);
+            }
+            catch (Exception ex)
+            {
+                //await EntitiesHelperClass.CloseLoading();
+                //await ViewServices.PopUpManager.PopErrorAsync($"Houve algum problema com a análise da foto dos alimentos. {Environment.NewLine} Retorno da API: {gptResult}. {Environment.NewLine}" + ExceptionManager.ExceptionMessage(ex));
+                //return;
+            }
+
+            try
+            {
+                AppDataHelperClass.GetStringTacoItemsBySimpleFoodItems(geminiSimpleFoodItems, out geminiFoodItems);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            await EntitiesHelperClass.CloseLoading();
+
+            bool gptOk = gptFoodItems != null && gptFoodItems.Count > 0;
+            bool geminiOk = geminiFoodItems != null && geminiFoodItems.Count > 0;
+
+            Console.WriteLine("ChatGPT: " + (gptOk ? "OK" : "Falha"));
+            Console.WriteLine("Gemini: " + (geminiOk ? "OK" : "Falha"));
+
+            if (gptOk)
+                return gptFoodItems;
+            else if (geminiOk)
+                return geminiFoodItems;
+            else
+                return null;
+        }
     }
 }
