ce16101fe5dda4ae1e823385cc78b5615bec555e
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-04-15 21:44:37 -0300

    Create AiResult.cs
    - Classe criada para concentrar os retornos do GPT e da OpenAI.

---
+++ NutriLensSolution/NutriLensClassLibrary/Models/AiResult.cs
@@ -0,0 +1,14 @@
+﻿namespace NutriLensClassLibrary.Models
+{
+    public class AiResult
+    {
+        public string GptResult { get; set; }
+        public string GeminiResult { get; set; }
+
+        public AiResult(string gptResult, string geminiResult)
+        {
+            GptResult = gptResult;
+            GeminiResult = geminiResult;
+        }
+    }
+}
