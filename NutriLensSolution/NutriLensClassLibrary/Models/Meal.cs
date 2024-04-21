2cbb7342dccbc5c377f8fc72a36d84bcc436649a
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-04-21 12:40:53 -0300

    Update Meal.cs

--- NutriLensSolution/NutriLensClassLibrary/Models/Meal.cs
+++ NutriLensSolution/NutriLensClassLibrary/Models/Meal.cs
@@ -1,4 +1,5 @@
 ﻿using NutriLensClassLibrary.Models;
+using System.Text;
 
 namespace NutriLensClassLibrary.Models
 {
@@ -22,6 +23,34 @@ namespace NutriLensClassLibrary.Models
         /// </summary>
         public List<FoodItem> FoodItems { get; set; }
 
+        public string DateInfo { get => DateTime.ToShortDateString(); }
+        public string TimeInfo { get => DateTime.ToShortTimeString(); }
+        public string FoodItemsCountInfo { get => FoodItems.Count.ToString(); }
+        public string FoodItemsInfo
+        {
+            get
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                foreach(FoodItem foodItem in FoodItems)
+                {
+                    if (foodItem.TacoFoodItem != null)
+                        stringBuilder.Append($"   - {foodItem.TacoFoodItem.Nome} ({foodItem.Portion} g - {foodItem.KiloCalorieInfo}){Environment.NewLine}");
+                    else
+                        stringBuilder.Append($"   - {foodItem.Name} ({foodItem.Portion} g - {foodItem.KiloCalorieInfo}){Environment.NewLine}");
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+        public string TotalKcalInfo
+        {
+            get => Math.Round(FoodItems.Select(x => x.KiloCalories).Sum(), 2).ToString() + " kcal";
+        }
+        public string TotalMassInfo
+        {
+            get => Math.Round(FoodItems.Select(x => double.Parse(x.Portion)).Sum(), 2).ToString() + " g";
+        }
         public override string ToString()
         {
             if(FoodItems.Count > 1)
