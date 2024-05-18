a9aa794385819e56c817363b6a4af10376a1eb39
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-05-18 16:45:07 -0300

    Create HashItem.cs
    - Classe para gerenciamento de hash dos controles de alteração dos dados de usuário no aplicativo.

---
+++ NutriLensSolution/NutriLensClassLibrary/Models/HashItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriLensClassLibrary.Models
+{
+    public class HashItem
+    {
+        public string Hash { get; set; }
+        public int ItemsCount { get; set; }
+    }
+}
