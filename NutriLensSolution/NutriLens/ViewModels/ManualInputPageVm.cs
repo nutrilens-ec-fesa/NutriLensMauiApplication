e9f6027ffedefa24db150b11311759befbf95344
Carlos Henrique <carlos_henrique97@outlook.com.br>
2024-06-12 10:33:45 -0300

    Verificações de DCNTs e intolerâncias

--- NutriLensSolution/NutriLens/ViewModels/ManualInputPageVm.cs
+++ NutriLensSolution/NutriLens/ViewModels/ManualInputPageVm.cs
@@ -2,11 +2,13 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NutriLens.Entities;
+using NutriLens.Models;
 using NutriLens.Services;
 using NutriLens.ViewInterfaces;
 using NutriLens.Views.Popups;
 using NutriLensClassLibrary.Models;
 using System.Collections.ObjectModel;
+using System.Text;
 
 namespace NutriLens.ViewModels
 {
@@ -145,6 +147,9 @@ namespace NutriLens.ViewModels
                     MealPicturePath = AppDataHelperClass.NewFoodPicturePath
                 };
 
+                if (!await DcntAndIntolerancesCheck(newMeal.FoodItems))
+                    return;
+
                 AppDataHelperClass.AddMeal(newMeal);
                 AppDataHelperClass.DetectedFoodItems?.Clear();
                 AppDataHelperClass.NewFoodPicturePath = string.Empty;
@@ -154,6 +159,9 @@ namespace NutriLens.ViewModels
             // Edição de uma refeição
             else
             {
+                if (!await DcntAndIntolerancesCheck(FoodItems.ToList()))
+                    return;
+
                 AppDataHelperClass.MealToEdit.FoodItems = FoodItems.ToList();
                 await AppDataHelperClass.UpdateMeal(AppDataHelperClass.MealToEdit);
                 AppDataHelperClass.MealToEdit = null;
@@ -190,7 +198,7 @@ namespace NutriLens.ViewModels
                 {
                     AppDataHelperClass.DetectedFoodItems = null;
 
-                    if(File.Exists(AppDataHelperClass.MealToEdit.MealPicturePath))
+                    if (File.Exists(AppDataHelperClass.MealToEdit.MealPicturePath))
                         AppDataHelperClass.NewFoodPicturePath = AppDataHelperClass.MealToEdit.MealPicturePath;
 
                     foreach (FoodItem food in AppDataHelperClass.MealToEdit.FoodItems)
@@ -222,7 +230,7 @@ namespace NutriLens.ViewModels
         [RelayCommand]
         private void Disappearing()
         {
-            
+
         }
 
         [RelayCommand]
@@ -284,6 +292,103 @@ namespace NutriLens.ViewModels
 
             OnPropertyChanged(nameof(FoodItemsQuantity));
             OnPropertyChanged(nameof(KiloCaloriesRound));
+        }
+
+        /// <summary>
+        /// Verifica os itens alimentícios da refeição e informa ao usuário caso haja algum item que interfira nas configurações
+        /// </summary>
+        /// <returns>Retorna verdadeiro caso o usuário tenha optado por continuar</returns>
+        private async Task<bool> DcntAndIntolerancesCheck(List<FoodItem> foodItems)
+        {
+            StringBuilder glutenWarning = new();
+
+            if (AppDataHelperClass.UserInfo.GlutenIntolerant)
+            {
+                List<FoodItem> glutenFoodItems = foodItems
+                    .Where(x => x.TacoFoodItem.Gluten != null && (bool)x.TacoFoodItem.Gluten)
+                    .ToList();
+
+                if (glutenFoodItems.Count > 0)
+                {
+                    glutenWarning.Append("Atenção! Os seguintes alimentos podem ter glúten em sua composição: " + Environment.NewLine);
+
+                    foreach (FoodItem glutenFoodItem in glutenFoodItems)
+                    {
+                        glutenWarning.Append(Environment.NewLine + glutenFoodItem.Name);
+                    }
+
+                    glutenWarning.Append(Environment.NewLine + Environment.NewLine + "Deseja continuar?");
+                }
+            }
+
+            StringBuilder lactoseWarning = new();
+
+            if (AppDataHelperClass.UserInfo.LactoseIntolerant)
+            {
+                List<FoodItem> lactoseFoodItems = foodItems
+                    .Where(x => x.TacoFoodItem.Lactose != null && (bool)x.TacoFoodItem.Lactose)
+                    .ToList();
+
+                if (lactoseFoodItems.Count > 0)
+                {
+                    lactoseWarning.Append("Atenção! Os seguintes alimentos podem ter lactose em sua composição: " + Environment.NewLine);
+
+                    foreach (FoodItem lactoseFoodItem in lactoseFoodItems)
+                    {
+                        lactoseWarning.Append(Environment.NewLine + lactoseFoodItem.Name);
+                    }
+
+                    lactoseWarning.Append(Environment.NewLine + Environment.NewLine + "Deseja continuar?");
+                }
+            }
+
+            MealListClass todayMeals = new MealListClass(AppDataHelperClass.GetTodayMeals());
+            MealListClass actualMeal = new MealListClass(new List<Meal> { new Meal { FoodItems = foodItems } });
+
+            if (AppDataHelperClass.UserInfo.Diabetes)
+            {
+                double todayTotalCarbohydratesConsumption = todayMeals.TotalCarbohydratesConsumption();
+                double actualMealCarbohydrates = actualMeal.TotalCarbohydratesConsumption();
+                double totalCarbohydratesAfterMeal = todayTotalCarbohydratesConsumption + actualMealCarbohydrates;
+
+                if (actualMealCarbohydrates > 0)
+                {
+                    if (todayTotalCarbohydratesConsumption > AppDataHelperClass.UserInfo.DailyCarbohydrateGoal)
+                    {
+                        if (!await ViewServices.PopUpManager.PopYesOrNoAsync("Aviso de diabetes", "Essa refeição possui carboidratos e você já ultrapassou o limite de carboidratos para hoje, deseja continuar?"))
+                            return false;
+                    }
+
+                    else if (totalCarbohydratesAfterMeal > AppDataHelperClass.UserInfo.DailyCarbohydrateGoal && !await ViewServices.PopUpManager.PopYesOrNoAsync("Aviso de diabetes", "Essa refeição possui carboidratos e, com essa refeição, vai ultrapassar o limite estabelecido, deseja continuar?"))
+                        return false;
+                }
+            }
+
+            if (AppDataHelperClass.UserInfo.Hipertension)
+            {
+                double todayTotalSodiumConsumption = todayMeals.TotalSodiumConsumption();
+                double actualMealSodium = actualMeal.TotalSodiumConsumption();
+                double totalSodiumAfterMeal = todayTotalSodiumConsumption + actualMealSodium;
+
+                if (actualMealSodium > 0)
+                {
+                    if (todayTotalSodiumConsumption > AppDataHelperClass.UserInfo.DailySodiumGoal)
+                    {
+                        if (!await ViewServices.PopUpManager.PopYesOrNoAsync("Aviso de hipertensão", "Essa refeição possui sódio e você já ultrapassou o limite de sódio para hoje, deseja continuar?"))
+                            return false;
+                    }
+                    else if (totalSodiumAfterMeal > AppDataHelperClass.UserInfo.DailySodiumGoal && !await ViewServices.PopUpManager.PopYesOrNoAsync("Aviso de hipertensão", "Essa refeição possui sódio e, com essa refeição, vai ultrapassar o limite estabelecido, deseja continuar?"))
+                        return false;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(glutenWarning.ToString()) && !await ViewServices.PopUpManager.PopYesOrNoAsync("Aviso de glúten", glutenWarning.ToString()))
+                return false;
+
+            if (!string.IsNullOrEmpty(lactoseWarning.ToString()) && !await ViewServices.PopUpManager.PopYesOrNoAsync("Aviso de lactose", lactoseWarning.ToString()))
+                return false;
+
+            return true;
         }
     }
 }
